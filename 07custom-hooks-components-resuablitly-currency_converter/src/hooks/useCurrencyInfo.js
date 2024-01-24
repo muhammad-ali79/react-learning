35e9792ed17e2ba47db188c9c8ab390a4e3871fb
muhammad-ali79 <alihelium369@gmail.com>
2024-01-24 20:40:24 +0500

    custom-hooks-components-resuability

--- 07custom-hooks-components-resuablitly-currency_converter/src/hooks/useCurrencyInfo.js
+++ 07custom-hooks-components-resuablitly-currency_converter/src/hooks/useCurrencyInfo.js
@@ -5,6 +5,8 @@ const useCurrencyInfo = (currency) => {
   // here we use the useState because we want to use this data in the dom
   const [data, setData] = useState({});
 
+  // let data;
+
   // here we use the useEffect hook because we want to run our callback whenever the currency dependecy changes
   useEffect(() => {
     // useEffect hook should not reuturn a promise so we make an iffe and immediately call it
@@ -12,11 +14,13 @@ const useCurrencyInfo = (currency) => {
       const request = await fetch(
         `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
       );
-      const data = await request.json();
-      setData(data[currency]);
+      const Apidata = await request.json();
+      setData(Apidata[currency]);
+
+      // if we do this then data will remain undefine becaus its react here state is not updated till we dont re-render the comonent using some hooks here like useState hook
+      // data = Apidata[currency];
     })();
   }, [currency]);
-
   return data;
 };
 
